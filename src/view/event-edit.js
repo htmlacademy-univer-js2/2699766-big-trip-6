564ea8b02727ab5dad5338c3072a94f0e11f0a9d
Терещенко Степан <tereschenkostep@gmail.com>
2026-06-08 20:25:26 +0500

    update event-edit.js

--- src/view/event-edit.js
+++ src/view/event-edit.js
@@ -101,10 +101,10 @@ export default class EventEditView extends AbstractStatefulView {
             </div>
             <div class="event__field-group event__field-group--time">
               <label class="visually-hidden" for="event-start-time-1">From</label>
-              <input class="event__input event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${dayjs(dateFrom).format('DD/MM/YY HH:mm')}">
+              <input class="event__input event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${dateFrom ? dayjs(dateFrom).format('DD/MM/YY HH:mm') : ''}">
               &mdash;
               <label class="visually-hidden" for="event-end-time-1">To</label>
-              <input class="event__input event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${dayjs(dateTo).format('DD/MM/YY HH:mm')}">
+              <input class="event__input event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${dateTo ? dayjs(dateTo).format('DD/MM/YY HH:mm') : ''}">
             </div>
             <div class="event__field-group event__field-group--price">
               <label class="event__label" for="event-price-1">
